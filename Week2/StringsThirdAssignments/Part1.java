92225543e4a476ba36ca86b822b52745e90276be
Nguyen Van Huy <57367674+nguyenvanhuy0612@users.noreply.github.com>
2019-11-20 00:43:53 +0700

    Update

--- Week2/StringsThirdAssignments/Part1.java
+++ Week2/StringsThirdAssignments/Part1.java
@@ -18,6 +18,7 @@ public class Part1 {
         //System.out.println("dna "+dna);
         return dna;
     }
+
     public int findStopIndex(String dna, int startIndex, String stopCodon){ //System.out.println("findStopIndex");
         int currIndex = dna.indexOf(stopCodon, startIndex + 3);
         while (currIndex != -1){
@@ -27,14 +28,17 @@ public class Part1 {
             }
             currIndex = dna.indexOf(stopCodon, currIndex + 1);
         }
-        return dna.length();
+        // return dna.length();
+        return -1;
     }
+
     public void testFindStopCodon(){
 
         String dna = "ATGCGTGATTAA";
         System.out.println("findStopIndex "+findStopIndex(dna, 0, "TAA"));
 
     }
+
     public String findGene(String dna, int beginIndex){
         int startIndex = dna.indexOf("ATG", beginIndex);
         if (startIndex == -1){
@@ -44,14 +48,29 @@ public class Part1 {
         int tagIndex = findStopIndex(dna, startIndex, "TAG");   //System.out.println("tagIndex "+tagIndex);
         int tgaIndex = findStopIndex(dna, startIndex, "TGA");   //System.out.println("tgaIndex "+tgaIndex);
 
-        int minIndex = Math.min(taaIndex, tagIndex);
-        minIndex = Math.min(minIndex, tgaIndex);               //System.out.println("minIndex "+minIndex);
+        // int minIndex = Math.min(taaIndex, tagIndex);
+        // minIndex = Math.min(minIndex, tgaIndex);               //System.out.println("minIndex "+minIndex);
 
-        if (minIndex == dna.length()){
+        // if (minIndex == dna.length()){
+            // return "";
+        // }
+        int minIndex = 0;
+        
+        if (taaIndex == -1 || (tagIndex != -1) && (tagIndex < taaIndex)){
+            minIndex = tagIndex;
+        }else{ minIndex = tagIndex; }
+        
+        if (minIndex == -1 || (tgaIndex != -1) && (tgaIndex < minIndex)){
+            minIndex = tgaIndex;
+        }
+        
+        if (minIndex == -1){
             return "";
         }
+        
         return dna.substring(startIndex, minIndex + 3);
     }
+
     public void testFindGene(){
         String dna = collectDNA();
         dna = dna.toUpperCase();
@@ -59,6 +78,7 @@ public class Part1 {
         System.out.println("dna.length() "+dna.length());
         System.out.println(findGene(dna, 0));
     }
+
     public void printAllGenes(String dna){
         int startIndex = dna.indexOf("ATG");
         while (startIndex != -1){
@@ -70,6 +90,7 @@ public class Part1 {
             startIndex = dna.indexOf(currGene, startIndex) + currGene.length();
         }
     }
+
     public StorageResource getAllGenes(String dna){
         StorageResource sr = new StorageResource();
         int startIndex = dna.indexOf("ATG");
@@ -103,8 +124,6 @@ public class Part1 {
         }   
     }
 }
-
-
 
 
 
