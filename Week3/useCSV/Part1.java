1cb37e5e19bf7b544c91082f0d18206465015066
Nguyen Van Huy <57367674+nguyenvanhuy0612@users.noreply.github.com>
2019-11-26 00:48:51 +0700

    add

--- Week3/useCSV/Part1.java
+++ Week3/useCSV/Part1.java
@@ -8,11 +8,24 @@
 import edu.duke.*;
 import org.apache.commons.csv.*;
 public class Part1 {
-
-    public CSVParser tester() {
+    
+    public void tester() {
         FileResource fr = new FileResource();
         CSVParser parser = fr.getCSVParser();
-        return parser;
+
+        //System.out.println(countryInfo(parser, "Malaysia"));
+
+        //parser = fr.getCSVParser();
+        //System.out.println(countryInfo(parser, "Germany"));
+        
+        //parser = fr.getCSVParser();
+        //listExportersTwoProducts(parser, "gold", "diamonds");
+        
+        //parser = fr.getCSVParser();
+        //System.out.println("numberOfExporters: " + numberOfExporters(parser, "gold"));
+        
+        parser = fr.getCSVParser();
+        bigExporters(parser, "$999,999,999");
     }
 
     public String countryInfo(CSVParser parser, String country) {
@@ -27,12 +40,39 @@ public class Part1 {
         return countryName;
     }
 
-    public void testCountryInfo() {
-        FileResource fr = new FileResource();
-        CSVParser parser = fr.getCSVParser();
-        
-        System.out.println(countryInfo(parser, "Malaysia"));
-        System.out.println(countryInfo(parser, "Germany"));
+    public void listExportersTwoProducts(CSVParser parser, String exportItem1, String exportItem2){
+        for (CSVRecord record : parser){
+            if (record.get("Exports").contains(exportItem1) && record.get("Exports").contains(exportItem2)){
+                System.out.println(record.get("Country"));
+            }
+        } 
     }
-
+    
+    public int numberOfExporters(CSVParser parser, String exportItem){
+        int count = 0;
+            for (CSVRecord record : parser){
+            if (record.get("Exports").contains(exportItem)){
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public Double convertToDouble(String input){
+        String currString = input.replace("$", "");
+        currString = currString.replace(",", "");
+        Double stringToNumber = Double.parseDouble(currString);
+        return stringToNumber;
+    }
+    
+    public void bigExporters(CSVParser parser, String amount){
+        double amountNum = convertToDouble(amount);
+        for (CSVRecord record : parser){
+            String valueItem = record.get("Value (dollars)");
+            double valueNum = convertToDouble(valueItem);
+            if (valueNum > amountNum){
+                System.out.println(record.get("Country")+": "+valueItem);
+            }
+        }
+    }
 }
