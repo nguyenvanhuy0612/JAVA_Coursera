2f62eeb16ec2a9592faddd35992ec55863446b4f
Nguyen Van Huy <57367674+nguyenvanhuy0612@users.noreply.github.com>
2019-12-09 15:00:46 +0700

    ađ

--- Week4/Births/BabyBirths.java
+++ Week4/Births/BabyBirths.java
@@ -16,10 +16,29 @@ public class BabyBirths {
             int numBorn = Integer.parseInt(rec.get(2));
             if (numBorn <= 100){
                 System.out.println("Name "+ rec.get(0) +
-                                   " Gender "+ rec.get(1) +
-                                   " Num Born "+ rec.get(2));
+                    " Gender "+ rec.get(1) +
+                    " Num Born "+ rec.get(2));
             }
         }
     }
 
+    public void totalBirths(FileResource fr ){
+        float totalBirths = 0;
+        float totalGirls = 0;
+        float totalBoys = 0;
+        for (CSVRecord rec : fr.getCSVParser(false)){
+            int numBorn = Integer.parseInt(rec.get(2));
+            if (rec.get(1).intern() == "f|F"){ //[fF]
+                totalGirls +=numBorn;
+            }else{
+                totalBoys += numBorn;
+            }
+        }
+        System.out.println("total births: "+totalBirths);
+    }
+
+    public void testTotalBirths(){
+        FileResource fr = new FileResource();
+        totalBirths(fr);
+    }
 }
